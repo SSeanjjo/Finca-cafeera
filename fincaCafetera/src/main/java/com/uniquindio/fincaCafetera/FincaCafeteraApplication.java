6eec291d76031cd453fbdf8d563db5939d9d3cea
SSeanjjo <142127854+SSeanjjo@users.noreply.github.com>
2026-02-28 11:49:06 -0500

    Project init

---
+++ fincaCafetera/src/main/java/com/uniquindio/fincaCafetera/FincaCafeteraApplication.java
@@ -0,0 +1,13 @@
+package com.uniquindio.fincaCafetera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincaCafeteraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincaCafeteraApplication.class, args);
+	}
+
+}
---
+++ fincaCafetera/src/test/java/com/uniquindio/fincaCafetera/FincaCafeteraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.fincaCafetera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincaCafeteraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
